9717bd704826d82081f1cd5e9d0cc3a1bdca06fe
bienmag <mvaretckaia@gmail.com>
2023-01-18 23:16:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function App ()
+{
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  var yyyy = today.getFullYear();
+  today = dd + '/' + mm + '/' + yyyy;
+
+
+  const [day, setDay] = useState()
+  const [month, setMonth] = useState()
+  const [year, setYear] = useState()
+  const [dateToday, setDateToday] = useState()
+
+
+  function collectDay (e)
+  {
+    setDay(e.target.value)
+    setDateToday(today)
+  }
+
+  function collectMonth (e)
+  {
+    setMonth(e.target.value)
+
+  }
+
+  function collectYear (e)
+  {
+    setYear(e.target.value)
+
+  }
+
+
+  function calculateWeeksBetween (d1, d2)
+  {
+    return Math.round((d2 - d1) / (7 * 24 * 60 * 60 * 1000));
+  }
+
+  function calculate (d1, d2)
+  {
+    let result = calculateWeeksBetween(new Date(today), new Date(day + month + year))
+    console.log(new Date(today))
+    console.log(day + month + year)
+    console.log(result)
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <p> Please enter your birth date</p>
+      <input type="number" placeholder="dd" onKeyUp={ collectDay }></input>
+      <input type="number" placeholder="mm" onKeyUp={ collectMonth }></input>
+      <input type="number" placeholder="yyyy" onKeyUp={ collectYear }></input>
+      <button onClick={ calculate }>Submit</button>
+    </div >
   );
 }
 
