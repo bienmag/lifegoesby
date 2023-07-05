39e608796a936bb75e485412804e9e2b584525ce
Margarita Varetckaia <mvaretckaia@gmail.com>
2023-07-05 22:37:16 +0200

    fix: number of weeks in 90 years lol

--- src/Weeks.js
+++ src/Weeks.js
@@ -2,7 +2,7 @@ import arrow from "./images/right-arrow.png";
 import rightArrow from "./images/right-arrow.png";
 
 function Weeks({ weeks }) {
-  const totalWeeks = 5200;
+  const totalWeeks = 4680;
   const templateWeeks = [];
   const GrayBox = (
     <div className="m-[1px] w-1 h-1 xl:w-2 xl:h-2 md:w-2 md:h-2 border-2 border-gray-300 xl:m-0.5" />
@@ -40,7 +40,7 @@ function Weeks({ weeks }) {
           <h1 className="text-amber-900"> Week of the Year</h1>
           <img className="pl-8 w-20" src={rightArrow} alt="Right arrow" />
         </div>
-        <div className=" w-[302px] sm:w-[300px] md:w-[500px]  xl:w-[624px] flex justify-between xl:pr-8 -ml-2 text-[10px] text-amber-900">
+        <div className=" w-[302px] sm:w-[300px] md:w-[500px]  xl:w-[624px] flex justify-between xl:pr-8 -ml-2 text-[8px] text-amber-900">
           <p>1</p>
           <p>5</p>
           <p>10</p>
@@ -59,26 +59,25 @@ function Weeks({ weeks }) {
           <h1 className="-rotate-90  text-amber-900">Age</h1>
           <img src={arrow} alt="arrow" className=" w-8 rotate-90 mt-6" />
         </div>
-        <div className="mr-0.5 -mt-1 -mb-1 flex flex-col justify-between text-[10px] text-amber-900  ">
-          <p>0</p>
-          <p>5</p>
-          <p>10</p>
-          <p>15</p>
-          <p>20</p>
-          <p>25</p>
-          <p>30</p>
-          <p>35</p>
-          <p>40</p>
-          <p>45</p>
-          <p>50</p>
-          <p>55</p>
-          <p>60</p>
-          <p>65</p>
-          <p>70</p>
-          <p>75</p>
-          <p>80</p>
-          <p>85</p>
-          <p>90</p>
+        <div className="mr-0.5 mb-12 flex flex-col  text-[8px] text-amber-900  ">
+          <p className="pb-12">0</p>
+          <p className="pb-12">5</p>
+          <p className="pb-12">10</p>
+          <p className="pb-12">15</p>
+          <p className="pb-12">20</p>
+          <p className="pb-12">25</p>
+          <p className="pb-12">30</p>
+          <p className="pb-12">35</p>
+          <p className="pb-12">40</p>
+          <p className="pb-12">45</p>
+          <p className="pb-12">50</p>
+          <p className="pb-12">55</p>
+          <p className="pb-12">60</p>
+          <p className="pb-12">65</p>
+          <p className="pb-12">70</p>
+          <p className="pb-12">75</p>
+          <p className="pb-12">80</p>
+          <p className="pb-12">85</p>
         </div>
         <div className="">{templateWeeks}</div>
       </div>
