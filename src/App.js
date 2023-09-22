ba95510564fea465b9cf7ffac8ede85ae96f469b
Margarita Varetckaia <mvaretckaia@gmail.com>
2023-09-22 13:00:19 +0200

    try to fix text center

--- src/App.js
+++ src/App.js
@@ -60,6 +60,7 @@ function App() {
                 className={`w-24 xl:w-40 mb-2  appearance-none outline-none bg-[#FCE5C2] text-amber-500 h-8 rounded-full text-center mr-2 px-2`}
                 type="number"
                 value={birthDate.month}
+                style={{ textAlignLast: 'center' }}
                 onChange={(e) =>
                   setBirthDate({ ...birthDate, month: e.target.value })
                 }
